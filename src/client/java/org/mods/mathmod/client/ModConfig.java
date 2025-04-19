35b3ef5709cb0aff661992c7a9e2de482e3f6f51
Visar <visarmullaj07@gmail.com>
2025-04-19 22:25:18 +0200

    Update default decimal precision

--- src/client/java/org/mods/mathmod/client/ModConfig.java
+++ src/client/java/org/mods/mathmod/client/ModConfig.java
@@ -2,5 +2,5 @@ package org.mods.mathmod.client;
 
 public class ModConfig {
   /** This controls the precision of the numbers after the decimal point.*/
-  public int decimalPrecision = 2;
+  public int decimalPrecision = 5;
 }
