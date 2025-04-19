428bedf1be2c40014427f23a5e068d2a917889f8
Visar <visarmullaj07@gmail.com>
2025-04-19 11:55:27 +0200

    division by zero error

--- src/client/java/org/mods/mathmod/client/MathmodClient.java
+++ src/client/java/org/mods/mathmod/client/MathmodClient.java
@@ -1,6 +1,7 @@
 package org.mods.mathmod.client;
 
 import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import net.fabricmc.api.ClientModInitializer;
 
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
@@ -8,8 +9,11 @@ import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallba
 import net.minecraft.text.Text;
 
 
-public class MathmodClient implements ClientModInitializer {
 
+
+public class MathmodClient implements ClientModInitializer {
+  private static final SimpleCommandExceptionType DIVIDE_BY_ZERO =
+      new SimpleCommandExceptionType(Text.literal("Cannot divide by zero."));
 
   @Override
   public void onInitializeClient() {
@@ -66,6 +70,9 @@ public class MathmodClient implements ClientModInitializer {
                           .executes(context -> {
                             int x = IntegerArgumentType.getInteger(context, "x");
                             int y = IntegerArgumentType.getInteger(context, "y");
+                            if (y == 0) {
+                              throw DIVIDE_BY_ZERO.create(); // 🔴 This shows a red error in chat
+                            }
                             context.getSource().sendFeedback(Text.literal("Result: " + (x / y)));
                             return 1;
                           })
