e7840c5f5cea90258e5c38321058f2d4f4767b2d
Visar <visarmullaj07@gmail.com>
2025-04-19 12:51:23 +0200

    Added decimal precision config

--- src/client/java/org/mods/mathmod/client/MathmodClient.java
+++ src/client/java/org/mods/mathmod/client/MathmodClient.java
@@ -1,5 +1,7 @@
 package org.mods.mathmod.client;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
@@ -7,13 +9,25 @@ import net.fabricmc.api.ClientModInitializer;
 
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.text.Text;
 
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.DecimalFormat;
+
 
 public class MathmodClient implements ClientModInitializer {
+  private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+  private static final Path CONFIG_PATH =
+      FabricLoader.getInstance().getConfigDir().resolve("mathmodConfing.json");
+
+  private static ModConfig config;
+
   private static final SimpleCommandExceptionType DIVIDE_BY_ZERO =
       new SimpleCommandExceptionType(Text.literal("Cannot divide by zero."));
   private static final SimpleCommandExceptionType INVALID_EXPR =
@@ -22,9 +36,26 @@ public class MathmodClient implements ClientModInitializer {
 
   @Override
   public void onInitializeClient() {
+    config = loadConfig();
+
     ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
       dispatcher.register(
           ClientCommandManager.literal("math")
+              .then(ClientCommandManager.literal("config")
+                  .then(ClientCommandManager.literal("precision")
+                      .then(ClientCommandManager.argument("value", IntegerArgumentType.integer(0))
+                          .executes(ctx -> {
+                            int v = IntegerArgumentType.getInteger(ctx, "value");
+                            config.decimalPrecision = v;
+                            saveConfig();
+                            ctx.getSource().sendFeedback(
+                                Text.literal("Decimal precision set to " + v)
+                            );
+                            return 1;
+                          })
+                      )
+                  )
+              )
               .then(ClientCommandManager.literal("add")
                   .then(ClientCommandManager.argument("x", IntegerArgumentType.integer())
                       .suggests(CommandUtils.suggestPlayerXZ)
@@ -87,33 +118,59 @@ public class MathmodClient implements ClientModInitializer {
               .then(ClientCommandManager.literal("eval")
                   .then(ClientCommandManager.argument("expression", StringArgumentType.greedyString())
                       .executes(context -> {
-                        String expr = StringArgumentType.getString(context, "expression");
-                        try {
-                          // Parse & evaluate
-                          Expression e = new ExpressionBuilder(expr)
-                              .variables("pi", "e")
-                              .build()
-                              // Bind them to their Math constants
-                              .setVariable("pi", Math.PI)
-                              .setVariable("e", Math.E);
+                            String expr = StringArgumentType.getString(context, "expression");
+                            try {
+                              // Parse & evaluate
+                              Expression e = new ExpressionBuilder(expr)
+                                  .variables("pi", "e")
+                                  .build()
+                                  // Bind them to their Math constants
+                                  .setVariable("pi", Math.PI)
+                                  .setVariable("e", Math.E);
 
-                          double result = e.evaluate();
-                          // Send result (trim .0 for integers)
-                          String out = (result == (long) result)
-                              ? Long.toString((long) result)
-                              : Double.toString(result);
-                          context.getSource().sendFeedback(
-                              Text.literal(expr + " = " + out)
-                          );
-                        } catch (Exception ex) {
-                          // On parse/eval error, show red error text
-                          throw INVALID_EXPR.create();
-                        }
+                              double result = e.evaluate();
 
-                        return 1;
-                      })))
+                              // Format using the decimal precision in the config
+                              DecimalFormat df = new DecimalFormat();
+                              df.setMaximumFractionDigits(config.decimalPrecision);
+                              String out = df.format(result);
+                              context.getSource()
+                                  .sendFeedback(Text.literal(expr + " = " + out));
+                            } catch (Exception ex) {
+                              // On parse/eval error, show red error text
+                              throw INVALID_EXPR.create();
+                            }
+
+                            return 1;
+                          }
+                      )
+                  )
+              )
       );
     });
   }
+  private ModConfig loadConfig() {
+    try {
+      if (Files.exists(CONFIG_PATH)) {
+        return GSON.fromJson(Files.newBufferedReader(CONFIG_PATH), ModConfig.class);
+      } else {
+        ModConfig cfg = new ModConfig();
+        Files.writeString(CONFIG_PATH, GSON.toJson(cfg));
+        return cfg;
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+      return new ModConfig(); // fallback
+    }
+  }
+
+  private void saveConfig() {
+    try {
+      Files.writeString(CONFIG_PATH, GSON.toJson(config));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
 }
 
---
+++ src/client/java/org/mods/mathmod/client/ModConfig.java
@@ -0,0 +1,6 @@
+package org.mods.mathmod.client;
+
+public class ModConfig {
+  /** how many fractional digits to show */
+  public int decimalPrecision = 2;
+}
