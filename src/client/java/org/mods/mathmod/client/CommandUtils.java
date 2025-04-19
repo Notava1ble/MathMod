16680b91a2ff2438dc3e02b0ba1a45ea7e186afd
Visar <visarmullaj07@gmail.com>
2025-04-19 22:25:48 +0200

    Another small commit

--- src/client/java/org/mods/mathmod/client/CommandUtils.java
+++ src/client/java/org/mods/mathmod/client/CommandUtils.java
@@ -5,7 +5,7 @@ import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.client.MinecraftClient;
 
 public class CommandUtils {
-  // Gives some suggestions to the user, or more specifically, the players X and Y coordinate
+  // Gives some suggestions to the user, or more specifically, the players X and Z coordinate
   public static final SuggestionProvider<FabricClientCommandSource> suggestPlayerXZ = (context, builder) -> {
     MinecraftClient client = MinecraftClient.getInstance();
 
